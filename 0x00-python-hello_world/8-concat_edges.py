0c1489ccabf5570d4355f4cdb71f13d65511d748
mkyd-kill <romeomureithi@gmail.com>
2023-10-30 08:23:24 -0700

    more string indices

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str = "Python is an interpreted, interactive, object-oriented programming\
+string = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str = str[39:66] + " " + str[107:111] + " " + str[0:7]
-print(str)
+string = string[39:67] + string[107:112] + string[0:7]
+print(string)
