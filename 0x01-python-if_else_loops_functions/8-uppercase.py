9eeda8bce95c98bde6a17d7f34d03a13799b39f4
mkyd-kill <romeomureithi@gmail.com>
2023-10-31 11:16:52 -0700

    lowercase to uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(string):
+    for i in string:
+        i = ord(i)
+        if i >= 97 and i <= 122:
+            i -= 32
+        i = chr(i)
+        print("%s" % i, end="")
+    print("")
