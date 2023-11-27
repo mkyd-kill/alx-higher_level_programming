355f26e0fc509db17bfd08ea618e53de333cbfcd
mkyd-kill <romeomureithi@gmail.com>
2023-11-27 04:39:19 -0800

    setter and getter methods

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -21,7 +21,7 @@ class Rectangle:
         """width Setter value method"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self._width = value
 
@@ -35,6 +35,6 @@ class Rectangle:
         """height Setter value method"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self._height = value
