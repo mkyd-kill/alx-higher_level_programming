afa9150a9d84c72349f10404d6de2e91343bdbd4
mkyd-kill <romeomureithi@gmail.com>
2023-11-27 04:41:44 -0800

    setter & getter

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -14,7 +14,7 @@ class Rectangle:
     @property
     def width(self):
         """width Getter method"""
-        return self._width
+        return self.__width
 
     @width.setter
     def width(self, value):
@@ -23,12 +23,12 @@ class Rectangle:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
-        self._width = value
+        self.__width = value
 
     @property
     def height(self):
         """height Getter method"""
-        return self._height
+        return self.__height
 
     @height.setter
     def height(self, value):
@@ -37,4 +37,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self._height = value
+        self.__height = value
