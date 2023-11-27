704ae7dad99a347b09a7bb2b8faa75254564efb5
mkyd-kill <romeomureithi@gmail.com>
2023-11-27 05:31:20 -0800

    static method

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -71,7 +71,7 @@ class Rectangle:
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
-    @staticmetod
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """returns the biggest rectangle based on the area"""
         if type(rect_1) is not Rectangle:
