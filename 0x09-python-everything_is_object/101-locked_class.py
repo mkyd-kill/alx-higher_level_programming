7967d732d3f36d3ee5727aa10ac45e6a5b7ff2cd
mkyd-kill <romeomureithi@gmail.com>
2023-11-28 09:39:05 -0800

    locked class

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-
-"""
-Class that prevents the user from dynamically creating
-new instance attributes
-"""
+"""Write a class LockedClass with no class or object attribute"""
 
 
 class LockedClass:
-    """prevents from creating user"""
-    __slots__ = ["firstname"]
+    """ Prevents the user from dynamically creating new instance attributes,
+    except if the new instance attribute is called first_name."""
+    __slots__ = ["first_name"]
