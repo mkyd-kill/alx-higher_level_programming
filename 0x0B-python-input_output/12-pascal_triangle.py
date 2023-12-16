703be3d4acfb1d25324cde6021eaa4e0218cfad2
Romeo Namunde <romeomureithi@gmail.com>
2023-12-17 01:59:29 +0300

    pascal triangle

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -6,17 +6,15 @@ Triangle of n
 
 
 def pascal_triangle(n: int):
+    """Pascal triangle list results"""
     result = [[1]]
 
     if n <= 0:
         return []
-    try:
-        for i in range(1, n):
-            row = [1]
-            for j in range(1, i):
-                row.append(result[i - 1][j - 1] + result[i - 1][j])
-            row.append(1)
-            result.append(row)
-    except ValueError:
-        pass
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            row.append(result[i - 1][j - 1] + result[i - 1][j])
+        row.append(1)
+        result.append(row)
     return result
