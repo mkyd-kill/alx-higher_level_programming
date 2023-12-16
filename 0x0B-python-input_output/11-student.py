f01c1fd76c61ff565e567c6194cfe0663c12c309
Romeo Namunde <romeomureithi@gmail.com>
2023-12-17 01:38:07 +0300

    replacing class object attributes

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Student class
+"""
+
+
+class Student:
+    """a class defining a student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves the obj representation"""
+        if attrs is None:
+            return self.__dict__
+        contents = {}
+        for i in attrs:
+            if i in self.__dict__ and type(i) is str:
+                contents[i] = self.__dict__.get(i)
+        return contents
+
+    def reload_from_json(self, json):
+        """Replaces all attributes of the class object"""
+        for i in json:
+            if i in self.__dict__:
+                self.__dict__[i] = json[i]
