af6523d74d1a21bebb5dfbc52c9da1cfa558f31e
Romeo Mureithi <romeomureithi@gmail.com>
2024-03-18 17:15:31 +0300

    simplified if

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,4 @@
 #!/usr/bin/node
 
-const process = require('process');
-let i = process.argv.length;
-
-if (i > 3)
-	console.log('Arguments found');
-else if (i == 2)
-	console.log('No argument');
-else if (i == 3)
-	console.log('Argument found');
+const args = process.argv.length;
+console.log(args == 2 ? 'No argument' : args == 3 ? 'Argument found' : 'Arguments found');
