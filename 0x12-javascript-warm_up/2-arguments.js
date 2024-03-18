e92249bbacd67bee9e82b3db2fb1e07d1102a650
Romeo Mureithi <romeomureithi@gmail.com>
2024-03-18 17:23:04 +0300

    ifs

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 
 const args = process.argv.length;
-console.log(args == 2 ? 'No argument' : args == 3 ? 'Argument found' : 'Arguments found');
+console.log(args === 2 ? 'No argument' : args === 3 ? 'Argument found' : 'Arguments found');
